3109b7094f00eaa71d1e2151af4d20bb05373e53
Nastia-Chorna <82803581+Nastia-Chorna@users.noreply.github.com>
2022-01-26 21:14:37 +0200

    started modal

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import { Component } from 'react';
-import Searchbar from './components/Searchbar/Searchbar';
-import ImageGallery from './components/ImageGallery/ImageGallery';
-import ImageGalleryItem from './components/ImageGalleryItem/ImageGalleryItem';
-import Loader from './components/Loader/Loader';
-import Button from './components/Button/Button';
+// import Searchbar from './components/Searchbar/Searchbar';
+// import ImageGallery from './components/ImageGallery/ImageGallery';
+// import ImageGalleryItem from './components/ImageGalleryItem/ImageGalleryItem';
+// import Loader from './components/Loader/Loader';
+// import Button from './components/Button/Button';
 import Modal from './components/Modal/Modal';
 
-state = {
-  
+
+class App extends Component {
+  state = {
+    filter: '',
+    showModal: false,
+  };
+
+  toggleModal = () => {
+    this.setState(({showModal}) => ({
+    showModal: !showModal
+  }))
+}
+
+
+  render() {
+    const { showModal } = this.state;
+    return (
+    <div>
+  {showModal&& <Modal />}
+      
+      </div>
+    )
+  };
 };
-
-render(){
-
-};
-
 export default App;
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,6 +1,38 @@
+import { Component } from "react";
 
-<div class="overlay">
-  <div class="modal">
-    <img src="" alt="" />
-  </div>
-</div>
+import PropTypes from "prop-types";
+
+class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener("keydown", this.handleKeydown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this.handleKeydown);
+  }
+
+  handleKeydown = (evt) => {
+    if (evt.code === "Escape") {
+      this.props.onToggle();
+    }
+  };
+
+  handleBackdropClick = (evt) => {
+    if (evt.currentTarget === evt.target) {
+      this.props.onToggle();
+    }
+  };
+
+  render() {
+    return (
+      <div onClick={this.handleBackdropClick}>
+        <div>{this.props.children}</div>
+      </div>
+    );
+  }
+}
+export default Modal;
+
+Modal.propTypes = {
+  onToggle: PropTypes.func.isRequired,
+};
